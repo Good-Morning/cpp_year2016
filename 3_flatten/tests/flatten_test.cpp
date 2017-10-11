fabdb268aa9fe8560497f7e997c5d0bd30c69a86
Eugene Nemchenko <n.eugene536@gmail.com>
2017-10-11 12:52:58 +0300

    add interface for flatten

--- 3_flatten/tests/flatten_test.cpp
+++ 3_flatten/tests/flatten_test.cpp
@@ -8,16 +8,4 @@
 #include <thread>
 #include <gtest/gtest.h>
 
-TEST(promise, IsReady) {
-//    Promise<int> promise;
-//    Future<int> f = promise.GetFuture();
-
-
-//    ASSERT_FALSE(f.IsReady());
-
-//    int const x = 10;
-//    promise.Set(x);
-//    ASSERT_TRUE(f.IsReady());
-}
-
 #endif // _GTEST

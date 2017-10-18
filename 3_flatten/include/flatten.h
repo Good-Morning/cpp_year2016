bd125b0a41f9f01b839fae9326f252f60e6eb8a4
Good-Morning <goood-morning@yandex.ru>
2017-10-18 23:58:02 +0300

    Update flatten.h

--- 3_flatten/include/flatten.h
+++ 3_flatten/include/flatten.h
@@ -3,14 +3,15 @@
 //
 #pragma once
 
+#include <CodeStyle.h>
 #include <future.h>
 #include <promise.h>
 
 template<typename T>
-Future<T> Flatten(Future<T> const &);
+Future<T> FLATTEN(Future<T> const &);
 
 template<typename T>
-Future<T> Flatten(Future<Future<T>> const &);
+Future<T> FLATTEN(Future<Future<T>> const &);
 
-template<template<typename> class C, typename T>
-Future<C<T>> Flatten(C<Future<T>> const &);
+template<template<typename, typename...> class C, typename T>
+Future<C<T>> FLATTEN(C<Future<T>> const &);
